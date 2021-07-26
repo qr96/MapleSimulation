bedc1f5d7a9f6b08b98a6571a8483a329a4d9f5f
qr96 <qr96@naver.com>
2021-07-26 22:26:15 +0900

    update

---
+++ app/src/main/java/com/example/maplesimulation/Cube.java
@@ -0,0 +1,19 @@
+package com.example.maplesimulation;
+
+public class Cube {
+    String[][] table = {
+            {"STR +12", "20"},
+            {"DEX +12", "20"},
+            {"INT +12", "20"},
+            {"LIK +12", "20"},
+            {"방어력 +12", "20"}
+    };
+
+    public String useCube() {
+        System.out.println("hello");
+
+        return "Error";
+    }
+
+    
+}
---
+++ app/src/main/java/com/example/maplesimulation/PotentialActivity.java
@@ -0,0 +1,25 @@
+package com.example.maplesimulation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PotentialActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_potential);
+    }
+
+    public void useCube(View view){
+        Cube cube = new Cube();
+        cube.useCube();
+    }
+
+    public void goBack(View view){
+        onBackPressed();
+    }
+
+}
+
